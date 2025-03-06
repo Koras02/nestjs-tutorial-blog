a3a5965901a463839a29e8be0004612e867dd42c
Koras02 <wjdgh0727@gmail.com>
2025-03-07 01:38:51 +0900

    1.Controller(컨트롤러)

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, CatsController],
+  providers: [AppService],
+})
+export class AppModule {}
