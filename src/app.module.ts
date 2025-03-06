5c2136293ce6b35dd47678bb733daf1654175487
Koras02 <wjdgh0727@gmail.com>
2025-03-07 03:27:08 +0900

    2.Provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
-/* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { Module } from "@nestjs/common";
+import { CatsModule } from "./cats/cats.module"; // 경로가 정확한지 확인하세요.
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  imports: [CatsModule], // CatsModule이 imports 배열에 포함되어 있어야 합니다.
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete } from "@nestjs/common";
+import { CatsService } from "./cats.service";
 
 @Controller("cats")
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  create(@Body() cat) {
+    this.catsService.create(cat);
+    return { message: "Cat created" };
+  }
+
   @Get()
-  findAll(): string {
-    return "This action returns all cats";
+  findAll() {
+    return this.catsService.findAll();
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string): string {
-    return `This action returns a cat with id #${id}`;
+  findOne(@Param("id") id: number) {
+    return this.catsService.findOne(id);
+  }
+
+  @Delete(":id")
+  delete(@Param("id") id: number) {
+    const result = this.catsService.delete(id);
+    if (result) {
+      return { message: "Cat deleted successfully" };
+    } else {
+      return { message: "Cat not found" };
+    }
   }
 }
