b759f0d8cc0ecc305d8bcdba10835ca3e11d15b7
Koras02 <wjdgh0727@gmail.com>
2025-08-21 20:33:35 +0900

    4.DI(의존성 주입)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,49 +1,12 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Param,
-  Body,
-} from "@nestjs/common";
-import { UsersService, User } from "./users.service";
+import { Controller, Get } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
-@Controller("users")
+@Controller("user")
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly userService: UsersService) {} // 자동 주입
 
   @Get()
-  getAllUsers(): User[] {
-    return this.usersService.getAll();
-  }
-
-  @Get(":id")
-  getOneUser(@Param("id") id: string): User | { error: string } {
-    const user = this.usersService.getOne(Number(id));
-    return user || { error: "User not found" };
-  }
-
-  @Post()
-  createUser(@Body("name") name: string, @Body("email") email: string): User {
-    return this.usersService.create(name, email);
-  }
-
-  @Put(":id")
-  updateUser(
-    @Param("id") id: string,
-    @Body("name") name?: string,
-    @Body("email") email?: string,
-  ): User | { error: string } {
-    const updated = this.usersService.update(Number(id), name, email);
-    return updated || { error: "User Not Found" };
-  }
-
-  @Delete(":id")
-  deleteUser(@Param("id") id: string): { message: string } {
-    const deleted = this.usersService.delete(Number(id));
-    return deleted
-      ? { message: "User deleted" }
-      : { message: "User not found" };
+  findAll() {
+    return this.userService.getUsers();
   }
 }
